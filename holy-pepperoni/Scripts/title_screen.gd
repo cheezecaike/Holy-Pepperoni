b0227399acd8a419c828ed315392fb3a7b9ace6b
cheezecaike <dilloncoburn123@gmail.com>
2025-02-18 16:21:55 -0600

    init commit of project files 02/18/25

---
+++ holy-pepperoni/Scripts/title_screen.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@onready var player = "res://Scenes/Entities/player_controller.tscn"
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	print("Start Button Pressed!")
+	get_tree().change_scene_to_file(player)
+
+func _on_quit_button_pressed() -> void:
+	print("Quit Button Pressed!")
+	get_tree().quit()
